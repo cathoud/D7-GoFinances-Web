114467559d53aa47d7302912c04ac7ab1df0deb6
Cathoud <cathoud@hotmail.com>
2020-07-06 11:32:30 -0300

    D7 Done

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
+    if (!uploadedFiles) return;
     // TODO
+    data.append('file', uploadedFiles[0].file, uploadedFiles[0].name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const newFiles: FileProps[] = files.map((file: File) => {
+      return {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      } as FileProps;
+    });
+    setUploadedFiles([...uploadedFiles, ...newFiles]);
   }
 
   return (
